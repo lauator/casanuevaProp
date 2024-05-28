20dad0aca50fec9ec08474c834c5597006b55c02
Lautaro Casanueva <casanuevalautaro@gmail.com>
2024-05-27 21:23:21 -0300

    tercer commit

---
+++ Scripts/buscadorPropiedades.js
@@ -0,0 +1,120 @@
+﻿    //public general
+    function search(sorting) {
+        var jsonData = getSearchParamenter();
+
+        var data = {
+            jsonData: JSON.stringify(jsonData),
+            sortOption: (sorting == "default") ? "" : getSortType(),
+            page: $("#PropertiesListSection .pagination a").html(),
+            pageSize: $("#items_per_page").val()
+        };
+        if (jsonData !== null) {
+            $.ajax({
+                type: "POST",
+                dataType: 'json',
+                url: 'FiltrarPropiedades',
+                data: data,
+                //contentType: "application/json; charset=utf-8",
+                success: function (result) {
+                    $('#PropertiesListSection').html(result.responseText);
+                },
+                error: function (result) {
+                    $('#PropertiesListSection').html(result.responseText);
+                    //console.log(result);
+                }
+            });
+        }
+    };
+
+    function searchPropFromIndex() {
+        var jsonData = getSearchParamenter();
+
+        var data = {
+            jsonData: JSON.stringify(jsonData)
+        };
+
+        if (jsonData !== null) {
+            $.ajax({
+                type: "POST",
+                dataType: 'json',
+                url: 'Home/FiltrarPropiedadesIndex',
+                data: data,
+                //contentType: "application/json; charset=utf-8",
+                success: function (result) {
+                    $('#PropertiesListSectionIndex').html(result.responseText);
+                },
+                error: function (result) {
+                    $('#PropertiesListSectionIndex').html(result.responseText);
+                }
+            });
+        }
+    };
+
+    function searchPropFromAdmin() {
+        var jsonData = getSearchParameterAdmin();
+
+        var data = {
+            jsonData: JSON.stringify(jsonData),
+            page: $("#PropertiesListSectionAdmin .pagination a").html(),
+            pageSize: $("#items_per_page").val()
+        };
+
+        if (jsonData !== null) {
+            $.ajax({
+                type: "POST",
+                dataType: 'json',
+                url: 'FiltrarPropiedadesAdministracion',
+                data: data,
+                //contentType: "application/json; charset=utf-8",
+                success: function (result) {
+                    $('#PropertiesListSectionAdmin').html(result.responseText);
+                },
+                error: function (result) {
+                    $('#PropertiesListSectionAdmin').html(result.responseText);
+                }
+            });
+        }
+    };
+    function getSortType() {
+        if ($("#sortByPrice").hasClass("active"))
+        { var pSortOption = "price_desc"; }
+        else
+        { var pSortOption = "price_asc"; }
+        return pSortOption;
+    };
+    function getSearchParamenter() {
+        var dataParameters = {
+            searchTextParam: $('#SearchString').val(),
+            idBarrio: $("#ddlBarrio").val(),
+            cantBanos: $("#ddlSanitarios").val(),
+            idTipoMoneda: $("#ddlTipoMoneda").val(),
+            cantAmbientes: $("#ddlAmbientes").val(),
+            idTipoAviso: $("#ddlTipoAviso").val(),
+
+            minPrecio: $("#txtPrecioMin").val(),
+            maxPrecio: $("#txtPrecioMax").val(),
+            metrosCubiertosMax: $("#txtMetrosCubiertosMax").val(),
+
+            conGarage: document.getElementById("conGarage").checked,
+            conAptoProf: document.getElementById("conAptoProf").checked,
+            conAgua: document.getElementById("conAgua").checked,
+            conLuz: document.getElementById("conLuz").checked,
+            conGas: document.getElementById("conGas").checked,
+            conInternet: document.getElementById("conInternet").checked,
+            conSegPriv: document.getElementById("conSeguridadPriv").checked,
+            conPorteroElec: document.getElementById("conPorteroElectrico").checked,
+            conAireAcon: document.getElementById("conAireAcondicionado").checked
+        };
+        return dataParameters;
+    };
+    function getSearchParameterAdmin() {
+        var dataParameters = {
+            codigoPropiedad: $("#CodigoPropiedad").val(),
+            searchTextParam: $('#SearchString').val(),
+            idEstadoAviso: $("#ddlEstadoAviso").val(),
+            idTipoAviso: $("#ddlTipoAviso").val(),
+            esDestacado: document.getElementById("esDestacado").checked
+        };
+        return dataParameters;
+    };
+
---
+++ production/Scripts/envioMailingPropiedades.js
@@ -0,0 +1,73 @@
+﻿function SendEmailForNewsLetter() {
+    var jsonData = {
+        FromEmail: $('#txtFromEmail').val(),
+        FromName: 'Visitante - Anónimo',
+        ToEmail: '',
+        Message: 'Deseo suscribirme al News letter para recibir notificaciones sobre nuevas propiedades.',
+        Subject: 'CASANUEVA - RECIBIR ACTUALIZACIONES Y NOTICIAS.'
+    };
+
+    var data = {
+        jsonData: JSON.stringify(jsonData)
+    };
+
+    EnviarMail(data);
+};
+function SendEmailForPropertieInfo(tituloPropiedad, descPropiedad) {
+    var idPropiedad = getParameterByName("idPropiedad");
+    var jsonData = {
+        FromEmail: $('#txtFromEmailInfoProp').val(),
+        FromName: 'Visitante - Anónimo',
+        ToEmail: '',
+        Message: "Deseo recibir información sobre la propiedad que estoy viendo. \n CODIGO DE PROPIEDAD: " + idPropiedad + "\n TITULO: " + tituloPropiedad + "\n DESCRIPCION: " + descPropiedad,
+        Subject: 'CASANUEVA - INFORMACIÓN SOBRE PROPIEDAD: ' + idPropiedad
+    };
+
+    var data = {
+        jsonData: JSON.stringify(jsonData)
+    };
+
+    EnviarMail(data);
+};
+function SendEmailContactForm(firstname, lastname, email, subject, message) {
+    var firstname = (firstname != '') ? firstname : 'Anonimo'
+    var lastname = (lastname != '') ? lastname : 'Anonimo';
+
+    var jsonData = {
+        FromEmail: email,
+        FromName: firstname.concat(', ', lastname),
+        ToEmail: '',
+        Message: message,
+        Subject: 'CASANUEVA - FORMULARIO DE CONTACTO: ' + subject
+    };
+
+    var data = {
+        jsonData: JSON.stringify(jsonData)
+    };
+
+    EnviarMail(data);
+};
+function EnviarMail(data) {
+    if (data !== null) {
+        $.ajax({
+            type: "POST",
+            async: true,
+            dataType: 'json',
+            url: 'Contact/EnviarMail',
+            data: data,
+            complete: function (result) {
+                $("#modalMailBodyText > p").text("Su mail ha sido enviado correctamente.");
+                $("#mailingModal").modal();
+            }
+        });
+    }
+};
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
